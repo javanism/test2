a2d69290511aaf098caad0f89e6353589b6640cf
javanism <javanism@hanmail.net>
2020-03-26 19:26:18 +0900

    commit2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/upload',require('./routes/uploadRouter'));
 
 const server=app.listen('8080',()=>{
-    console.log('8080 server ready....^^');
+    console.log('8080 server ready....^^***');
 });
 
 const a=require('./socket');
