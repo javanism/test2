63da333bab433e66f0d4c64f722f98a3ea3d16e5
javanism <javanism@hanmail.net>
2020-03-27 10:01:44 +0900

    commit_jes

---
+++ server2.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/upload',require('./routes/uploadRouter'));
+
+const server=app.listen('8082',()=>{
+    console.log('8082 server ready....^^***');
+});
+
+const a=require('./socket');
+a(server);
+
+const b=require('./sse');
+b(server);
+
